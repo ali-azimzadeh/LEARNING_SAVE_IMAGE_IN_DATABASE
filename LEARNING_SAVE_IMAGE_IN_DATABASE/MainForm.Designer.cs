4e65535ae978a8459ed71a9f3dd7b0c9ccb315de
ali azimzadeh <aliazimzadeh@gmail.com>
2022-08-27 11:03:23 +0430

    first commit

---
+++ LEARNING_SAVE_IMAGE_IN_DATABASE/MainForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace LEARNING_SAVE_IMAGE_IN_DATABASE
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.myPictureBox = new System.Windows.Forms.PictureBox();
+            this.BrowsButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.OpenButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.myPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(12, 239);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(374, 30);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // myPictureBox
+            // 
+            this.myPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.myPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.myPictureBox.Name = "myPictureBox";
+            this.myPictureBox.Size = new System.Drawing.Size(374, 185);
+            this.myPictureBox.TabIndex = 1;
+            this.myPictureBox.TabStop = false;
+            // 
+            // BrowsButton
+            // 
+            this.BrowsButton.Location = new System.Drawing.Point(12, 203);
+            this.BrowsButton.Name = "BrowsButton";
+            this.BrowsButton.Size = new System.Drawing.Size(374, 30);
+            this.BrowsButton.TabIndex = 2;
+            this.BrowsButton.Text = "Brows";
+            this.BrowsButton.UseVisualStyleBackColor = true;
+            this.BrowsButton.Click += new System.EventHandler(this.BrowsButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(12, 275);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(374, 30);
+            this.OpenButton.TabIndex = 3;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 312);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.BrowsButton);
+            this.Controls.Add(this.myPictureBox);
+            this.Controls.Add(this.SaveButton);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.myPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.PictureBox myPictureBox;
+        private System.Windows.Forms.Button BrowsButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button OpenButton;
+    }
+}
---
+++ LEARNING_SAVE_IMAGE_IN_DATABASE/MainForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LEARNING_SAVE_IMAGE_IN_DATABASE
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            MyDataSetTableAdapters.PersonTableAdapter personAdapter =
+                new MyDataSetTableAdapters.PersonTableAdapter();
+
+            byte[] myImage = 
+                ImageToByteArray(myPictureBox.Image);
+
+            personAdapter.Insert(Id: 100, Avatar: myImage);
+        }
+
+        public byte[] ImageToByteArray(System.Drawing.Image image)
+        {
+            MemoryStream ms = new MemoryStream();
+            image.Save(ms, image.RawFormat);
+            return ms.ToArray();
+
+            //using (var ms = new MemoryStream())
+            //{
+            //    image.Save(ms, image.RawFormat);
+            //    return ms.ToArray();
+            //}
+        }
+
+        private void BrowsButton_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                myPictureBox.Image = 
+                    Image.FromFile(openFileDialog.FileName);
+            }
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            MyDataSetTableAdapters.PersonTableAdapter personAdapter =
+               new MyDataSetTableAdapters.PersonTableAdapter();
+        
+            Image image = 
+                GetImageFromByteArray(personAdapter.GetData()[0].Avatar);
+
+            myPictureBox.Image = image;
+        }
+
+        public static Bitmap GetImageFromByteArray(byte[] byteArray)
+        {
+            ImageConverter im = new ImageConverter();
+
+            Bitmap bm = (Bitmap)im.ConvertFrom(byteArray);
+
+            return bm;
+        }
+    }
+}
